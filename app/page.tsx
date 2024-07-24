3dfbce3e923210cd4f29cc30709adbf6b44e139b
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-24 21:20:23 +0530

    applied basic tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
-        <h1> hii there</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="text-yellow-50"> hii there</h1>
       </div>
     </main>
   );
