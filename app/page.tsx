3c67e70889e008c143a60af78988b7ee4eb12fcf
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-25 13:07:08 +0530

    adjusted dots

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     // <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    //   <div className="max-w-7xl w-full">
-    //     <Hero />
-    //   </div>
+    // <div className="max-w-7xl w-full">
+    //   <Hero />
+    // </div>
     // </main>
     <BackgroundGradientAnimation>
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <div className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          <Hero />
+        <div className=" drop-shadow-4xl ">
+          <div className="max-w-7xl w-full">
+            <Hero />
+          </div>
+          {/* <Hero /> */}
           {/* <h1> hii idiot</h1> */}
         </div>
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,14 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div className="h-screen top-0 -left-0 w-full dark:bg-transparent  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] absolute flex items-center justify-center">
-        {/* <div
-        className="h-screen w-screen items-center justify-center bg-transparent
+      <div
+        className="h-screen w-full dark:bg-transparent bg-white 
        dark:bg-dot-white/[0.19] bg-dot-black/[0.2] absolute 
-       flex  bottom-0 right-0 top-0 -left-0"
-      > */}
+       flex items-center justify-center top-0 left-0"
+      >
         <div
           className="absolute pointer-events-none inset-0 flex 
-          items-center justify-center
+          items-center justify-center dark:bg-transparent bg-white
          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
