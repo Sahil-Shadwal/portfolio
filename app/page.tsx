d47ebb87fd2128148af66b7e45402db5f6edeca5
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-26 00:16:39 +0530

    grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+import { FaH } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -13,7 +17,11 @@ export default function Home() {
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
         <div className=" drop-shadow-4xl ">
           <div className="max-w-7xl w-full">
+            <FloatingNav
+              navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+            />
             <Hero />
+            <Grid />
           </div>
           {/* <Hero /> */}
           {/* <h1> hii idiot</h1> */}
