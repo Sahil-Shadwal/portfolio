4d3ce8cdb00af8d3023f8dc360fae8c308b01ebb
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-25 12:58:16 +0530

    blob animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Hero from "@/components/Hero";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero />
+    // <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    //   <div className="max-w-7xl w-full">
+    //     <Hero />
+    //   </div>
+    // </main>
+    <BackgroundGradientAnimation>
+      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+        <div className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+          <Hero />
+          {/* <h1> hii idiot</h1> */}
+        </div>
       </div>
-    </main>
+    </BackgroundGradientAnimation>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,25 +5,15 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vh]"
-          fill="purple"
-        />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue" />
-      </div>
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white 
+      <div className="h-screen top-0 -left-0 w-full dark:bg-transparent  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] absolute flex items-center justify-center">
+        {/* <div
+        className="h-screen w-screen items-center justify-center bg-transparent
        dark:bg-dot-white/[0.19] bg-dot-black/[0.2] absolute 
-       flex items-center justify-center top-0 left-0"
-      >
+       flex  bottom-0 right-0 top-0 -left-0"
+      > */}
         <div
           className="absolute pointer-events-none inset-0 flex 
-          items-center justify-center dark:bg-black-100 bg-white
+          items-center justify-center
          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
