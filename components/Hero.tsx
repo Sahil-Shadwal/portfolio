78358f1b200594033d17264a433efa994cd5beec
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-25 13:14:16 +0530

    fixed top margin

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-30">
       <div
         className="h-screen w-full dark:bg-transparent bg-white 
        dark:bg-dot-white/[0.19] bg-dot-black/[0.2] absolute 
