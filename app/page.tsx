484cf070f3dd399ebd7bd267eb7fb7feb98e9f1e
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-27 00:56:56 +0530

    hydration errorrrrrrr

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
@@ -22,6 +23,7 @@ export default function Home() {
             <FloatingNav navItems={navItems} />
             <Hero />
             <Grid />
+            <RecentProjects />
           </div>
         </div>
       </div>
